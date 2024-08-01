74afc1924bac7d032e04145b58aceadc3ce8068c
FYPC7 <75959821@est.unap.edu.pe>
2024-08-01 00:46:28 -0500

    Configuracion para mostrar los préstamos

--- prestamos.py
+++ prestamos.py
@@ -6,3 +6,11 @@ import pandas as pd
 class PrestamosApp(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.tree = ttk.Treeview(self, columns=('idprestamo', 'idlibro', 'idusuario', 'fecha_prestamo', 'fecha_devolucion'), show='headings')
+        self.tree.heading('idprestamo', text='ID')
+        self.tree.heading('idlibro', text='ID Libro')
+        self.tree.heading('idusuario', text='ID Usuario')
+        self.tree.heading('fecha_prestamo', text='Fecha Prestamo')
+        self.tree.heading('fecha_devolucion', text='Fecha Devolucion')
+        self.tree.pack(expand=True, fill='both')
