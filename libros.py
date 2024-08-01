18326bef22b269925a322a25f46b49fb6884dda9
Adelmi195 <74399160@est.unap.edu.pe>
2024-08-01 01:23:27 -0500

    configuracion de la tabla

--- libros.py
+++ libros.py
@@ -25,3 +25,17 @@ class LibrosApp(ttk.Frame):
         self.style.map('TButton',
                        background=[('active', '#45a049')],
                        relief=[('pressed', 'sunken')])
+
+        self.tree = ttk.Treeview(self, columns=('idlibro', 'TITULO', 'idcategoria', 'AUTOR', 'ISBN'), show='headings')
+        self.tree.heading('idlibro', text='ID')
+        self.tree.heading('TITULO', text='Título')
+        self.tree.heading('AUTOR', text='Autor')
+        self.tree.heading('idcategoria', text='ID Categoría')
+        self.tree.heading('ISBN', text='ISBN')
+        self.tree.column('idlibro', width=50)
+        self.tree.column('TITULO', width=200)
+        self.tree.column('AUTOR', width=150)
+        self.tree.column('idcategoria', width=100)
+        self.tree.column('ISBN', width=150)
+        self.tree.pack(expand=True, fill='both', padx=10, pady=10)
+
