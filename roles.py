9420e5b7bc11a4e9b021acd86d5b3d2ae489f2a0
FYPC7 <75959821@est.unap.edu.pe>
2024-08-01 00:48:05 -0500

    Se muestra lista de roles
    Se creo una vista de árbol para mostrar la lista de roles

--- roles.py
+++ roles.py
@@ -5,3 +5,16 @@ from tkinter import ttk, messagebox
 class RolesApp(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.tree = ttk.Treeview(self, columns=('idrol', 'NOMBRE'), show='headings')
+        self.tree.heading('idrol', text='ID')
+        self.tree.heading('NOMBRE', text='Nombre')
+        self.tree.pack(expand=True, fill='both')
+
+        self.form_frame = ttk.Frame(self)
+        self.form_frame.pack(pady=10)
+
+        self.nombre_label = ttk.Label(self.form_frame, text="Nombre:")
+        self.nombre_label.grid(row=0, column=0)
+        self.nombre_entry = ttk.Entry(self.form_frame)
+        self.nombre_entry.grid(row=0, column=1)
