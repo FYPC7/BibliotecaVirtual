0c87ee5171f2b0c82c653aed609fc3b6b6f32775
FYPC7 <75959821@est.unap.edu.pe>
2024-08-01 00:39:37 -0500

    Se inicializa la clase

--- categorias.py
+++ categorias.py
@@ -6,4 +6,8 @@ class CategoriasApp(ttk.Frame):
 
     def __init__(self, parent):
         super().__init__(parent)
+        self.setup_ui()
+        self.create_table()
+        self.populate_tree()
+        self.populate_initial_data()
     
