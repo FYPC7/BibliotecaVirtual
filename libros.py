54984054ef12f78364371288f6950251a13f352d
FYPC7 <75959821@est.unap.edu.pe>
2024-08-01 00:42:57 -0500

    Configuración del estilo

--- libros.py
+++ libros.py
@@ -6,3 +6,22 @@ import pandas as pd
 class LibrosApp(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.style = ttk.Style()
+        self.style.configure('Treeview',
+                             background='#f0f0f0',
+                             foreground='#333333',
+                             fieldbackground='#e6e6e6',
+                             font=('Helvetica', 12))
+        self.style.configure('Treeview.Heading',
+                             background='#4CAF50',
+                             foreground='white',
+                             font=('Helvetica', 14, 'bold'))
+        self.style.configure('TButton',
+                             relief='flat',
+                             background='#4CAF50',
+                             foreground='white',
+                             font=('Helvetica', 12, 'bold'))
+        self.style.map('TButton',
+                       background=[('active', '#45a049')],
+                       relief=[('pressed', 'sunken')])
