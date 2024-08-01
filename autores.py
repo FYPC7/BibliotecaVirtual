156a372c6de12d88b2a2db7b9609a0151eee3078
FYPC7 <75959821@est.unap.edu.pe>
2024-08-01 00:36:16 -0500

    Inicialización del marco
    Inicialización del marco y del árbol de vista

--- autores.py
+++ autores.py
@@ -6,3 +6,11 @@ import pandas as pd
 class AutoresApp(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+        
+        self.tree = ttk.Treeview(self, columns=('idautor', 'NOMBRES', 'APELLIDOS', 'DNI', 'NACIONALIDAD'), show='headings')
+        self.tree.heading('idautor', text='ID')
+        self.tree.heading('NOMBRES', text='Nombres')
+        self.tree.heading('APELLIDOS', text='Apellidos')
+        self.tree.heading('DNI', text='DNI')
+        self.tree.heading('NACIONALIDAD', text='Nacionalidad')
+        self.tree.pack(expand=True, fill='both', padx=10, pady=10)
