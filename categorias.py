6dc6b3fb35a4dd9bcc526f9ee5d54ae1df7a8d79
Adelmi195 <74399160@est.unap.edu.pe>
2024-08-01 02:47:44 -0500

    Función para editar categoría

--- categorias.py
+++ categorias.py
@@ -92,3 +92,21 @@ class CategoriasApp(ttk.Frame):
             self.populate_tree()
         else:
             messagebox.showerror("Error", "El campo nombre es obligatorio")
+
+    def edit_categoria(self):
+        selected_item = self.tree.selection()
+        if selected_item:
+            nombre = self.nombre_entry.get()
+            categoria_id = self.tree.item(selected_item)['values'][0]
+
+            if nombre:
+                self.execute_query('''
+                    UPDATE categorias SET NOMBRE=? WHERE idcategoria=?
+                ''', (nombre, categoria_id))
+                messagebox.showinfo("Éxito", "Categoría actualizada correctamente")
+                self.populate_tree()
+                self.clear_form()
+            else:
+                messagebox.showerror("Error", "El campo nombre es obligatorio")
+        else:
+            messagebox.showerror("Error", "Por favor, selecciona una categoría para editar")
